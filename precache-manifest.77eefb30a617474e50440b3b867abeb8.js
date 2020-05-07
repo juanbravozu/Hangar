7e8ecb31e84ee344416f7ca34e531e659ffe49e7
juanbravozu <juanda.bravo98@gmail.com>
2020-05-07 14:50:59 -0500

    Updates

---
+++ precache-manifest.77eefb30a617474e50440b3b867abeb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b7d6382adc65d3227356d65166a52a",
+    "url": "/Hangar/index.html"
+  },
+  {
+    "revision": "937a9b26a220cdaceea1",
+    "url": "/Hangar/static/css/main.5b752e9c.chunk.css"
+  },
+  {
+    "revision": "e94f0cbc07b5aa08ba94",
+    "url": "/Hangar/static/js/2.4dd10535.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Hangar/static/js/2.4dd10535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937a9b26a220cdaceea1",
+    "url": "/Hangar/static/js/main.c753cfef.chunk.js"
+  },
+  {
+    "revision": "9abe4b6ecb952683b912",
+    "url": "/Hangar/static/js/runtime-main.f2a9ecf9.js"
+  }
+]);
